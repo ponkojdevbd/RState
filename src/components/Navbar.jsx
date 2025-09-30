3a7e7dcf422aab66e7882d990d4d6d3088fab45e
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2025-09-30 21:11:31 +0600

    Update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,26 +7,38 @@ const Navbar = () => {
       <div className=" container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
         <img src={assets.logo} alt="Logo" />
 
-        <ul className=" hidden md:flex gap-7 text-white">
-          <li>
-            <a href="#Header" className=" cursor-pointer hover:text-gray-400">
-              Home
-            </a>
-            <a href="#About" className=" cursor-pointer hover:text-gray-400">
-              About
-            </a>
-            <a href="#Projects" className=" cursor-pointer hover:text-gray-400">
-              Projects
-            </a>
-            <a
-              href="#Testimonials"
-              className=" cursor-pointer hover:text-gray-400"
-            >
-              Testimonials
-            </a>
-          </li>
-        </ul>
-        <button className="hidden md:block cursor-pointer bg-white px-8 py-2">
+        {/* Desktop Menu */}
+        <div className="">
+          <ul className=" hidden md:flex gap-7 items-center justify-between text-white">
+            <li>
+              <a href="#Header" className=" cursor-pointer hover:text-gray-400">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#About" className=" cursor-pointer hover:text-gray-400">
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#Projects"
+                className=" cursor-pointer hover:text-gray-400"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="#Testimonials"
+                className=" cursor-pointer hover:text-gray-400"
+              >
+                Testimonials
+              </a>
+            </li>
+          </ul>
+        </div>
+        <button className="hidden md:block cursor-pointer bg-white px-8 py-2 rounded">
           Sign up
         </button>
       </div>
